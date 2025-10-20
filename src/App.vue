ff5e943e1ba4bce2f5b6a291643c18d5e64662de
Angelo Mari H. Manlangit <manlangitangelomh@gmail.com>
2025-10-20 13:38:08 +0800

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,11 @@
-<<<<<<< HEAD
-<script setup>
-import loginForm from './components/login-form.vue';
-</script>
+<script setup></script>
 
 <template>
-  <router-view />
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
 </template>
 
-<style >
-
-=======
-<template>
-  <LoginForm />
-</template>
-<script>
-import LoginForm from './components/LoginForm.vue'
-
-export default {
-  name: 'App',
-  components: {
-    LoginForm
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
->>>>>>> 8059d13620da166e7220c345e7bbb66c23fdcaef
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-<<<<<<< HEAD
-import router from './router.js'
+import router from './router'
 
+const app = createApp(App)
 
-createApp(App).use(router).mount('#app');
-=======
+app.use(router)
 
-createApp(App).mount('#app')
->>>>>>> 8059d13620da166e7220c345e7bbb66c23fdcaef
+app.mount('#app')
