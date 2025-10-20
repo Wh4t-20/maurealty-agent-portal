ee420a9d3d67f45b49c1d78c3c18b8b152fedd98
Angelo Mari H. Manlangit <manlangitangelomh@gmail.com>
2025-10-20 13:47:56 +0800

    Fixed routing

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <LoginForm />
 </template>
 
-<style scoped></style>
+<script>
+import LoginForm from './components/LoginForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LoginForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginForm from '@/components/LoginForm.vue'
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/login',
+  },
+  {
+    path: '/login',
+    name: 'LoginForm',
+    component: LoginForm,
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
 })
 
 export default router
