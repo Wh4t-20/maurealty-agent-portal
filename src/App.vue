7c7bf5aef0e6c2eaa21169c7ecfe61633bce6da3
Angelo Mari H. Manlangit <manlangitangelomh@gmail.com>
2025-10-28 22:39:05 +0800

    Typescript compatibility

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,7 @@
-<template>
-  <test />
+<script setup lang="ts"></script>
 
-  <LoginForm />
+<template>
+  <h1 class="text-3xl font-bold underline">HELLO</h1>
 </template>
 
-<script>
-import LoginForm from './components/LoginForm.vue'
-import test from './components/test.vue'
-
-export default {
-  name: 'App',
-  components: {
-    LoginForm,
-    test,
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped></style>
