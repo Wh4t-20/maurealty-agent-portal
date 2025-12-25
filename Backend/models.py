ac7bf68ed778f29bde077b165629edbfd5f261f5
raretutal <retutal.ryananthony@gmail.com>
2025-12-25 14:41:51 +0800

    added temporary schema, will finalize later

--- Backend/models.py
+++ Backend/models.py
@@ -1,3 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class PropertyType(db.Model):
+    __tablename__ = 'property_type_ID_table' # need new naming??
+    property_type_ID = db.Column(db.Integer, primary_key = True)
+    property_type = db.Column(db.String(50), nullable = False) # placeholder
+
+class ListingsTable(db.Model):
+    __tablename__ = 'Listings'
+    # will link up with agent_ID soon
+    listing_ID = db.Column(db.Integer, primary_key = True)
+    listing_title = db.Column(db.String(255), nullable = False)
+    bedroom_count = db.Column(db.Integer, default = 0)
+    bathroom_count = db.Column(db.Integer, default = 0)
+    property_type_ID = db.Column(db.Integer, db.ForeignKey('property_type_ID_table.property_type_ID'))
+    floor_area = db.Column(db.Numeric(10, 2))
+    lot_area = db.Column(db.Numeric(10,2))
+    price = db.Column(db.Numeric(15,2), nullable = False)
+    commission = db.Column(db.Numeric(5,2))
+    location = db.Column(db.String(255))
+    description = db.Column(db.Text)
+    
+     
