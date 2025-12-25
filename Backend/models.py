02608f4e7ad921c8fa4e3ea297e9cf6e7dc00404
raretutal <retutal.ryananthony@gmail.com>
2025-12-25 14:53:35 +0800

    added table for url of images (not finished)

--- Backend/models.py
+++ Backend/models.py
@@ -3,9 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class PropertyType(db.Model):
-    __tablename__ = 'property_type_ID_table' # need new naming??
+    __tablename__ = 'PropertyTypeIDTable' # need new naming??
     property_type_ID = db.Column(db.Integer, primary_key = True)
     property_type = db.Column(db.String(50), nullable = False) # placeholder
+
+class ListingImageTable(db.Model):
+    __tablename__ = 'ListingImages'
+    listing_ID = db.Column(db.Integer, db.ForeignKey('Listings.listing_ID')) 
+    # image ID
+    # is_thumbnail
 
 class ListingsTable(db.Model):
     __tablename__ = 'Listings'
@@ -14,12 +20,12 @@ class ListingsTable(db.Model):
     listing_title = db.Column(db.String(255), nullable = False)
     bedroom_count = db.Column(db.Integer, default = 0)
     bathroom_count = db.Column(db.Integer, default = 0)
-    property_type_ID = db.Column(db.Integer, db.ForeignKey('property_type_ID_table.property_type_ID'))
+    property_type_ID = db.Column(db.Integer, db.ForeignKey('PropertyTypeIDTable.property_type_ID'))
     floor_area = db.Column(db.Numeric(10, 2))
     lot_area = db.Column(db.Numeric(10,2))
     price = db.Column(db.Numeric(15,2), nullable = False)
     commission = db.Column(db.Numeric(5,2))
     location = db.Column(db.String(255))
     description = db.Column(db.Text)
-    
-     
+
+
