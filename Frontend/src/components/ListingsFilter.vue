5008b752ef027e11fc6a9e89a8d4214cc0459a05
Angelo Manlangit <manlangitangelomh@gmail.com>
2026-04-02 22:11:59 +0800

    Listbox anims

--- Frontend/src/components/ListingsFilter.vue
+++ Frontend/src/components/ListingsFilter.vue
@@ -9,9 +9,12 @@
         </ListboxButton>
 
         <transition
-          leave-active-class="transition duration-100 ease-in"
-          leave-from-class="opacity-100"
-          leave-to-class="opacity-0"
+          enter-active-class="transition duration-100 ease-out"
+            enter-from-class="transform scale-95 opacity-0"
+            enter-to-class="transform scale-100 opacity-100"
+            leave-active-class="transition duration-75 ease-out"
+            leave-from-class="transform scale-100 opacity-100"
+            leave-to-class="transform scale-95 opacity-0"
         >
           <ListboxOptions
             class="absolute mt-1 max-h-60 w-fit min-w-full overflow-auto rounded-xl bg-[#ECF1F5] py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none"
