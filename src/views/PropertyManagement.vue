4769e7656f267e52b6ea31b2ba0f3790cd575815
dextr5 <dexterico007@gmail.com>
2026-04-20 21:49:28 +0800

    import new service

--- src/views/PropertyManagement.vue
+++ src/views/PropertyManagement.vue
@@ -374,6 +374,7 @@
 import { ref, watch } from 'vue';
 import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/vue'
 import type { HouseAndLot, Lot, Condominium, Memorial } from '@/assets/classes/listings';
+import { listingsService } from '@/services/listingsServices'; // <-- Import your new service!
 
 // Combine all interfaces for the form state
 type PropertyForm = HouseAndLot & Lot & Condominium & Memorial;
