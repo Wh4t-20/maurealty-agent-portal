37d5f9e42cf0cd7bab6b1c38dbf485c718470591
lmrkive <licastanos@up.edu.ph>
2026-04-24 09:34:40 +0800

    fixed property-card

--- src/components/PropertyCard.vue
+++ src/components/PropertyCard.vue
@@ -1,23 +1,29 @@
 <template>
   <div class="relative w-full max-w-150"> 
     <div class = "relative">
-    <img :src="houseimg" alt="House" 
-      class="w-full aspect-square object-cover rounded-lg border border-gray-300" 
-    />
-    <span :class="['absolute top-3 right-3 text-sm font-light text-white px-3 rounded-full tracking-wider shadow-md',propertyTypeColor(details.property_type)]">
-  {{ formattedPropertyType(details.property_type) }}
-</span>
+      <img :src="houseimg" alt="House" 
+        class="w-full aspect-square object-cover rounded-lg border border-gray-300" 
+        />
+    <span :class="['absolute top-3 right-3 text-sm font-light text-white px-3 py-1 rounded-full tracking-wider shadow-md',propertyTypeColor(details.property_type)]">
+      {{ formattedPropertyType(details.property_type) }}
+    </span>
     </div>
-    <div class="z-1 -mt-10 w-9/10 p-3 px-5  rounded-lg bg-white border border-gray-300 shadow-md/30 ">
+    
+    <div class="flex flex-col h-full z-1 -mt-10 w-9/10 p-3 px-5  rounded-lg bg-white border border-gray-300 shadow-md/30 ">
       <p class="text-xl text-maurealty-blue text-balance font-semibold tracking-tight ">{{ details.listing_title }}</p>
       
       <span class="block bg-maurealty-green break-all font-normal text-lg text-white px-3 rounded-full tracking-wider mr-2 mt-0.5">
         ₱ {{ details.price.toLocaleString() }}
       </span>
-    
-      <p class="text-base text-black text-balance tracking-tight mt-5 ">{{ details.location }}</p>
-      <p class="text-sm text-gray-500 mt-1">Created at: {{ details.created_at.toLocaleDateString() }}</p>
-      <p class="tracking-wide">{{ details.developer_name }}</p>
+      
+      <div class="flex items-center gap-1 text-black mt-2">
+        <MapPin class="w-4 h-4" />
+        <span class="text-sm">
+            {{ details.location }}
+        </span>
+      </div>
+      <p class="tracking-wide mb-0">{{ details.developer_name }}</p>
+      <p class=" text-sm text-gray-500 mt-auto">Created at: {{ details.created_at.toLocaleDateString() }}</p>
     </div>
   </div>
 </template>
@@ -26,7 +32,7 @@
 import { computed } from 'vue' // Don't forget to import this
 import { type Property }  from '@/assets/classes/listings'
 import house1 from '@/assets/images/sample-house.jpg' // Kept as a fallback image
-
+import { MapPin } from 'lucide-vue-next'
 const props = defineProps<{ details: Property }>()
 
 function formattedPropertyType (type: string) {
@@ -49,7 +55,7 @@ function formattedPropertyType (type: string) {
 function propertyTypeColor(type: string) {
   switch (type) {
     case 'house_and_lot':
-      return 'bg-green-600'
+      return 'bg-[#1b5c2d]'
     case 'lot_only':
       return 'bg-yellow-500'
     case 'condominium':
