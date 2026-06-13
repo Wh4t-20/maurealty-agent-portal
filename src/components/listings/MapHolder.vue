9d1a418b7cc7740e34897a8f55a62abc605797e7
Angelo Manlangit <manlangitangelomh@gmail.com>
2026-06-13 10:22:54 +0800

    MARKERS

--- src/components/listings/MapHolder.vue
+++ src/components/listings/MapHolder.vue
@@ -10,6 +10,9 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 const mapContainer = ref(null);
 const map = ref(null);
 
+// marker stuff
+const marker = ref(null);
+
 onMounted(() => {
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN;
 
@@ -19,11 +22,25 @@ onMounted(() => {
     container: mapContainer.value,
     style: 'mapbox://styles/mapbox/streets-v12',
     center: [123.8854, 10.3157],
-    zoom: 12
+    zoom: 15
   });
+
+  marker.value = new mapboxgl.Marker({
+    color: 'red'
+  }).setLngLat([123.8854, 10.3157])
+    .addTo(map.value);
 });
 
+const clearMarker = () => {
+  if (marker.value) {
+    marker.value.remove();
+    marker.value = null;
+  }
+};
+
 onUnmounted(() => {
+  clearMarker();
+
   if (map.value) {
     map.value.remove();
     map.value = null;
