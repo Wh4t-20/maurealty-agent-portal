52d9aa9b5ac6c1e5741e497f357cc73ee519055f
Angelo Manlangit <manlangitangelomh@gmail.com>
2026-06-22 18:16:52 +0800

    New office hours format

--- src/assets/classes/developers.ts
+++ src/assets/classes/developers.ts
@@ -15,3 +15,20 @@ export interface DayOption {
     name: string;
     shortcut: string;
 }
+
+export const days: DayOption[] = [
+    { id: 1, name: 'Monday', shortcut: 'Mon' },
+    { id: 2, name: 'Tuesday', shortcut: 'Tue' },
+    { id: 3, name: 'Wednesday', shortcut: 'Wed' },
+    { id: 4, name: 'Thursday', shortcut: 'Thu' },
+    { id: 5, name: 'Friday', shortcut: 'Fri' },
+    { id: 6, name: 'Saturday', shortcut: 'Sat' },
+    { id: 7, name: 'Sunday', shortcut: 'Sun' },
+    { id: 8, name: 'Holiday', shortcut: 'Hol'}
+]
+
+export interface OfficeHourSlot {
+    selectedDays: DayOption[];
+    openTime: string;
+    closeTime: string;
+}
