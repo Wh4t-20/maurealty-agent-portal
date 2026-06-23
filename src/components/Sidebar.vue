e90afba47358e85fbd0656a123b160fc35405be2
Angelo Manlangit <manlangitangelomh@gmail.com>
2026-06-23 22:54:23 +0800

    Added badge on sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,87 +1,92 @@
 <template>
-    <aside
-      class=" top-0 left-0 h-screen bg-maurealty-blue 
-             flex flex-col py-6 shadow-lg
-             transition-all duration-300
-             w-20 hover:w-90 group overflow-hidden sticky"
-    >
-    <router-link to="/profile" class="flex flex-col items-center">
-      <img src="@/assets/images/Maurealty.png" alt="MauRealty Logo" class="w-3/5 h-auto " />
-      <img :src="profileImage" alt="Agent"
-        class="w-3/7 aspect-square object-cover rounded-full outline-2 outline-white m-3" />
+  <aside
+    class=" top-0 left-0 h-screen bg-maurealty-blue 
+            flex flex-col py-6 shadow-lg
+            transition-all duration-300
+            w-20 hover:w-90 group overflow-hidden sticky"
+  >
+  <router-link to="/profile" class="flex flex-col items-center">
+    <img src="@/assets/images/Maurealty.png" alt="MauRealty Logo" class="w-3/5 h-auto " />
+    <img :src="profileImage" alt="Agent"
+      class="w-3/7 aspect-square object-cover rounded-full outline-2 outline-white m-3" />
 
+    <span class="flex items-center gap-1.5">
       <p class="group-hover:text-xl text-0 opacity-0 group-hover:opacity-100 transition whitespace-nowrap text-white text-[clamp(0.5rem,2vw,1rem)]">
         {{ agentName }}
       </p>
-
-      <p class="group-hover:text-xl text-0 opacity-0 group-hover:opacity-100 transition whitespace-nowrap text-white mb-3">
-        {{ agentPosition }}
-      </p>
-    </router-link>
+      <img v-if="agent.admin_access" :src="MauBadge" alt="Admin Badge" class="opacity-0 group-hover:opacity-100 size-4.75 transition whitespace-nowrap"/>
+    </span>
     
-      <!-- TOP NAV -->
 
-      <div class="flex flex-col overflow-y-auto space-y-2 px-3">
-        <router-link
-          v-for="item in topItems"
-          :key="item.id"
-          :to="item.path"
-          class="flex items-center  text-white text-[clamp(0.5rem,2vw,1rem)] font-light
-                 p-3 rounded-lg hover:bg-[#2A3242] transition"
+    <p class="group-hover:text-xl text-0 opacity-0 group-hover:opacity-100 transition whitespace-nowrap text-white mb-3">
+      {{ agentPosition }}
+    </p>
+  </router-link>
+  
+    <!-- TOP NAV -->
+
+    <div class="flex flex-col overflow-y-auto space-y-2 px-3">
+      <router-link
+        v-for="item in topItems"
+        :key="item.id"
+        :to="item.path"
+        class="flex items-center  text-white text-[clamp(0.5rem,2vw,1rem)] font-light
+                p-3 rounded-lg hover:bg-[#2A3242] transition"
+      >
+        <component :is="item.icon" class="w-6 h-5 shrink-0 " :stroke-width="1"  />
+        <!-- LABEL -->
+        <span
+          class="ml-4 whitespace-nowrap opacity-0 
+                  group-hover:opacity-100 transition"
         >
-          <component :is="item.icon" class="w-6 h-5 shrink-0 " :stroke-width="1"  />
-          <!-- LABEL -->
-          <span
-            class="ml-4 whitespace-nowrap opacity-0 
-                   group-hover:opacity-100 transition"
-          >
-            {{ item.label }}
-          </span>
-        </router-link>
-      </div>
-  
-      <!-- LOGOUT -->
-      <div class="mt-auto px-3">
-        <button
-          @click="handleLogout"
-          class="flex items-center text-white
-                 p-3 rounded-lg hover:bg-[#2A3242] transition cursor-pointer group"
+          {{ item.label }}
+        </span>
+      </router-link>
+    </div>
+
+    <!-- LOGOUT -->
+    <div class="mt-auto px-3">
+      <button
+        @click="handleLogout"
+        class="flex items-center text-white
+                p-3 rounded-lg hover:bg-[#2A3242] transition cursor-pointer group"
+      >
+        <LogOut class="w-7 h-7 shrink-0" :stroke-width="1"/>
+
+        <span
+          class="ml-4 whitespace-nowrap opacity-0 text-[clamp(0.5rem,2vw,1rem) font-light
+                  group-hover:opacity-100 transition"
         >
-          <LogOut class="w-7 h-7 shrink-0" :stroke-width="1"/>
-  
-          <span
-            class="ml-4 whitespace-nowrap opacity-0 text-[clamp(0.5rem,2vw,1rem) font-light
-                   group-hover:opacity-100 transition"
-          >
-            Logout
-          </span>
-        </button>
-      </div>
-    </aside>
-  </template>
-  
-  <script lang="ts" setup>
-  import { ref, computed, onMounted } from 'vue';
-  import { useRouter } from 'vue-router';
-  import { authService } from '@/services/authService';
-  import { agentService } from '@/services/agentService';
-  import placeholder from '@/assets/images/default_placeholder.png';
+          Logout
+        </span>
+      </button>
+    </div>
+  </aside>
+</template>
 
-  const router = useRouter();
-  const agent = ref<any>(null);
+<script lang="ts" setup>
+import { ref, computed, onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import { authService } from '@/services/authService';
+import { agentService } from '@/services/agentService';
+import placeholder from '@/assets/images/default_placeholder.png';
+import MauBadge from '@/assets/images/MauBadge.svg'
 
-  import {
-    LayoutDashboard,
-    LayoutList,
-    Calculator,
-    Ticket,
-    Mail,
-    Trophy,
-    ContactRound,
-    LogOut,
-    SquareChartGantt,
-    ReceiptText
-  } from "lucide-vue-next";
+const router = useRouter();
+const agent = ref<any>(null);
+
+import {
+  LayoutDashboard,
+  LayoutList,
+  Calculator,
+  Ticket,
+  Mail,
+  Trophy,
+  ContactRound,
+  LogOut,
+  SquareChartGantt,
+  ReceiptText
+} from "lucide-vue-next";
 
 // fetch current Agent profile data
 onMounted(async () => {
