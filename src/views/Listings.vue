6a8918ddf44f8c06ead0718a869484753651f3ed
raretutal <retutal.ryananthony@gmail.com>
2026-08-11 00:34:59 +0800

    added a watch function that actively monitors props.details?.image_url to make it possible to allow image skeleton to persist for the few extra milliseconds it takes the browser to download the final image.

--- src/views/Listings.vue
+++ src/views/Listings.vue
@@ -140,7 +140,7 @@
                 v-for="n in itemsPerPage" 
                 :key="'skeleton-' + n" 
                 :isLoading="true" 
-                class="flex flex-col items-center w-full"
+                class="flex flex-col items-center hover:-translate-y-2 hover:scale-105 hover:z-5 transition-all cursor-pointer"
               />
             </template>
             <!-- 2. Render actual cards when data is ready -->
